cbef32638bea26055c7e46653f297ece37e73990
Anshita Aishwarya <101793943+anshi1995@users.noreply.github.com>
2023-01-07 22:35:04 -0500

    Add files via upload

---
+++ Table_Insertion/category.sql
@@ -0,0 +1,22 @@
+use ecommerce_db;
+
+INSERT INTO Category VALUES
+(400,'Headphone','A pair of padded speakers which you wear over your ears in order to listen to a radio or recorded music.'),
+(401,'Phone','Portable device for connecting to a telecommunications network in order to transmit and receive voice, video, or other data.'),
+(402,'Phone Accessories ','Any hardware that is not integral to the operation of a mobile smartphone as designed by the manufacturer'),
+(403,'Speaker','Device for converting electrical energy into acoustical signal energy that is radiated into a room or open air.'),
+(404,'Charging Cable','A cable used to charge a smartphone or other portable device.'),
+(405,'Watch','A portable timepiece intended to be carried or worn by a person.'),
+(406,'Watch Accessories','Any hardware that is not integral to the operation of a watch as designed by the manufacturer'),
+(407,'Laptop','A small, portable personal computer'),
+(408,'Laptop Accessories','Any hardware that is not integral to the operation of a laptop as designed by the manufacturer'),
+(409,'Pen Drive','A plug-and-play portable storage device that uses flash memory and is lightweight'),
+(410,'External Hard Disk','A portable storage device that can be attached to a computer through a USB or FireWire connection, or wirelessly. '),
+(411,'Monitor','An electronic output device that is also known as a video display terminal (VDT) or a video display unit (VDU).'),
+(412,'AA Battery','A single cell measures 49.2‚Äì50.5 mm (1.94‚Äì1.99 in) in length.'),
+(413,'AAA Battery','A single cell that measures 10.5 mm (0.41 in) in diameter and 44.5 mm (1.75 in) in length.'),
+(414,'TV','A telecommunication medium for transmitting moving images and sound.'),
+(415,'Home Theater','An entertainment system for the home that usually consists of a large television with video components (such as a DVD player and VCR) and an audio system offering surround sound.'),
+(416,'Aux Cable','An auxiliary cable used to connect any external device to mobile phones, tablets etc. '),
+(417,'Car Electronics Accessories','Electronic systems used in vehicles, including engine management, ignition, radio, carputers, telematics, in-car entertainment systems, and others.'),
+(418,'Fitness Accessories ','Best fitness gadgets to power up your daily workout.');
